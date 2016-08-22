f04bbb1b9a16731d4452e8b3c1e6c9773faeb533
zhouwenjun1 <w614449035@163.com>
2016-08-23 00:05:14 +0800

    add web class

---
+++ accounting-web/src/main/java/com/zit/accounting/web/controller/api/GatewayController.java
@@ -0,0 +1,14 @@
+package com.zit.accounting.web.controller.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author zhou 2016/08/22
+ */
+@Controller
+public class GatewayController {
+	
+	private static final Logger _LOG = LoggerFactory.getLogger(GatewayController.class);
+}
